13d057ef4142d540cd906c43fdb6a65915c9612d
João Otávio da Silva <joao.otavio1225@gmail.com>
2022-05-11 14:19:27 -0300

    uploading the projects

---
+++ bankline-android/app/src/main/java/me/dio/bankline/data/BanklineRepository.kt
@@ -0,0 +1,29 @@
+package me.dio.bankline.data
+
+import android.util.Log
+import androidx.lifecycle.liveData
+import me.dio.bankline.data.remote.BanklineApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object BanklineRepository {
+
+    private val TAG = javaClass.simpleName
+
+    private val restApi = Retrofit.Builder()
+        .baseUrl("https://api-bankline-dio-v1.herokuapp.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(BanklineApi::class.java)
+
+    fun findBankStatement(accountHolderId: Int) = liveData {
+        emit(State.Wait)
+        try {
+            emit(State.Success(data = restApi.findBankStatement(accountHolderId)))
+        } catch (e: Exception) {
+            Log.e(TAG, e.message, e)
+            emit(State.Error(e.message))
+        }
+    }
+
+}
